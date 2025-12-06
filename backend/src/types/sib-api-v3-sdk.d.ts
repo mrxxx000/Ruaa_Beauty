dfa7d2f29769ebb3cb8bac3baa8a711838158a42
mrxxx000 <akmalsafi43@gmail.com>
2025-12-06 04:01:07 +0100

    fix

--- backend/src/types/sib-api-v3-sdk.d.ts
+++ backend/src/types/sib-api-v3-sdk.d.ts
@@ -1,14 +1,13 @@
 declare module 'sib-api-v3-sdk' {
   export class ApiClient {
-    static instance: ApiClient;
-    authentications: {
-      'api-key': {
-        apiKey: string;
-      };
-    };
+    static instance: any;
+    authentications: any;
   }
 
   export class TransactionalEmailsApi {
     sendTransacEmail(emailData: any): Promise<any>;
   }
+
+  const _default: any;
+  export default _default;
 }
